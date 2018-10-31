90d507f9c6b137c3a6a18780d7893a22029e9fb5
ymp-kakeru <yamashita.kakeru392@mail.kyutech.jp>
2018-10-31 15:43:54 +0900

    samplinger complete

---
+++ mod01_cntl/src/imu_samplinger_new.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include "ros/ros.h"
+#include "sensor_msgs/Imu.h"
+#include <std_msgs/String.h>
+
+class Imu_samplinger
+{
+public:
+  Imu_samplinger();
+
+private:
+  void imuCb(const sensor_msgs::Imu::ConstPtr& imu);
+//  void timeCb(const ros::TimerEvent& event);
+  int count ;
+  double SampTime,SamplingTime ;
+  std::string FileName = "test.csv";
+
+  ros::NodeHandle nh_;
+  ros::Subscriber imu_sub;
+};
+
+Imu_samplinger::Imu_samplinger():
+  count(0),SamplingTime(5)
+{
+  nh_.getParam("SampTime",SamplingTime);
+  nh_.getParam("FileName",FileName);
+
+  ROS_INFO("Sampling Now %d",count) ;
+
+  ros::Rate rate(SampTime);
+  while(ros::ok){
+    imu_sub = nh_.subscribe<sensor_msgs::Imu>("hokuyo3d/imu",10,&Imu_samplinger::imuCb,this);
+    rate.sleep();
+  }
+}
+
+/*void Imu_samplinger::timeCb(const ros::TimerEvent& event)
+{
+  ros::Timer e = nh_.createTimer(ros::Duration(SampTime),&Imu_samplinger::imuCb);
+}*/
+
+void Imu_samplinger::imuCb(const sensor_msgs::Imu::ConstPtr& imu)
+{
+  sensor_msgs::Imu imu_data;
+  float time = count / SampTime;
+
+  std::ofstream ofs(FileName.c_str(),std::ios::app);
+  if(count==0){
+    ofs << "time","time_cpu","x","y","z","\n" ;;
+  }else{
+    ofs << time,imu_data.linear_acceleration,"\n" ;
+  }
+  ofs.close();
+  count++;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "imu_samplinger");
+  Imu_samplinger imu_samplinger;
+  ros::spin();
+}
